bdd34a78ac6dc44989da37c8e45c59ae9519030d
larajunho <larajunho@gmail.com>
2025-09-12 20:23:10 -0300

    fix:fix route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
-namespace :api do
-  namespace :v1 do
-    resources :menus, only: [:index, :show] do
-      resources :menu_items, only: [:index, :show]
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :menus, only: [:index, :show] do
+        resources :menu_items, only: [:index, :show]
+      end
     end
   end
 end
