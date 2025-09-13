371a0365f6d6622c838bbd9e99dc4b328730a36e
larajunho <larajunho@gmail.com>
2025-09-12 21:06:23 -0300

    chore: generate Restaurant  model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_11_233114) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_12_235738) do
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
@@ -21,6 +21,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_233114) do
   end
 
   create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
